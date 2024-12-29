7c09e214c71cb26596ad1b8301131a5aebad48b5
Ілля Овсянніков <il.ovsiannikov@fora.ua>
2024-12-29 21:20:54 +0200

    create user component and heaeder

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  select = output<string>();
+
+  /*
+    @Output() select = new EventEmitter();
+  */
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectUser(): void {
+    this.select.emit(this.id);
+  }
+}
